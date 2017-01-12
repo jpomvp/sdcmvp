9afe56d36fe7c6219f446f923e897849d8228789
robertrittmuller <robert.rittmuller@dot.gov>
2017-01-12 15:48:32 -0500

    Fixed an issue in Glue implementation. Page rendering was broken using the defaults.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const BabelRegister = require('babel-core/register');
+/*const BabelRegister = require('babel-core/register');
 const Glue = require('glue');
 const Manifest = require('./manifest');
 
@@ -14,5 +14,20 @@ const composeOptions = {
     }
 };
 
+module.exports = Glue.compose.bind(Glue, Manifest.get('/'), composeOptions);*/
+
+var Glue = require('glue');
+var Manifest = require('./manifest');
+
+
+var composeOptions = {
+    relativeTo: __dirname,
+    preRegister: function(server, next){
+        require('babel-core/register')({
+            presets: ['react', 'es2015']
+        });
+        next();
+    }
+};
 
 module.exports = Glue.compose.bind(Glue, Manifest.get('/'), composeOptions);

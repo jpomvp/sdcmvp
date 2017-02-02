12a8a1b2fed1737203c94376132ae60c186bb7b3
robertrittmuller <robert.rittmuller@dot.gov>
2017-02-01 19:08:01 -0500

    Prototype grid output for container display.

--- client/pages/account/home.jsx
+++ client/pages/account/home.jsx
@@ -1,54 +1,78 @@
 'use strict';
-const Moment = require('moment');
 const React = require('react');
+const ReactRouter = require('react-router');
 
-
+const Link = ReactRouter.Link;
 class HomePage extends React.Component {
+
     constructor(props) {
 
         super(props);
 
-        this.state = this.getThisMoment();
     }
 
-    componentDidMount() {
+    onEditClick() {
 
-        this.interval = setInterval(this.refreshTime.bind(this), 1000);
-    }
+        // Open edit instance page
 
-    componentWillUnmount() {
-
-        clearInterval(this.interval);
-    }
-
-    refreshTime() {
-
-        this.setState(this.getThisMoment());
-    }
-
-    getThisMoment() {
-
-        const thisMoment = Moment();
-
-        return {
-            second: thisMoment.format('ss'),
-            minute: thisMoment.format('mm'),
-            hour: thisMoment.format('HH'),
-            day: thisMoment.format('DD'),
-            month: thisMoment.format('MM'),
-            year: thisMoment.format('YYYY')
-        };
     }
 
     render() {
 
         return (
+
             <section className="section-home container">
                 <div className="row">
                     <div className="col-sm-7">
                         <h2 className="page-header">MY SDC ACCOUNT</h2>
                         <div className="row">
 
+                        </div>
+                        <div className="row">
+                            <div className="table-responsive">
+                                <table className="table table-striped table-results">
+                                    <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th className="stretch">Instance Name</th>
+                                        <th>Status</th>
+                                        <th>ID</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <button
+                                                    className="btn btn-default btn-sm"
+                                                    onClick={this.onEditClick.bind(this)}>
+                                                    Edit
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <Link to="/instances">
+                                                Test Instance
+                                                </Link>
+                                            </td>
+                                            <td>Running</td>
+                                            <td>12345</td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <button
+                                                    className="btn btn-default btn-sm"
+                                                    onClick={this.onEditClick.bind(this)}>
+                                                    Edit
+                                                </button>
+                                            </td>
+                                            <td>Test Instance 2</td>
+                                            <td>Stopped</td>
+                                            <td>536454</td>
+                                        </tr>
+                                        <tr></tr>
+                                        <tr></tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                     <div className="col-sm-5">
